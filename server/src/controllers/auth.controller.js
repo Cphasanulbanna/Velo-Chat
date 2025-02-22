18f816e6ef5c402197c984b8861d3a7b5e039be9
Cphasanulbanna <cphasanulbanna@gmail.com>
2025-02-22 21:37:11 +0530

    created auth routes

---
+++ server/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = async (req, res) => {
+  res.render("signup");
+};
+
+export const login = async (req, res) => {
+  res.render("login");
+};
+
+export const logout = async (req, res) => {
+  res.render("logout");
+};
--- server/src/index.js
+++ server/src/index.js
@@ -4,7 +4,7 @@ import authRoutes from "./routes/auth.route.js";
 
 const app = express();
 
-app.arguments("/api/auth", authRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(5001, () => {
   console.log(`Server is running on: http://localhost:5001`);
